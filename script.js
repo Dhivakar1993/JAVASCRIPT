d91a7c7d518ec97882118c868b21f285dc7781bd
Dhivakar1993 <dhivadit@gmail.com>
2024-04-30 20:02:49 +0530

    Day 5 task

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-document.getElementById("Dhiva").textContent="Hello, world";
+let myResume={
+    "basics": {
+      "name": "Dhivakar M",
+      "email": "dhivadit@gamil.com",
+      "phone": 9786475024,
+      "degree": "DIT",
+      "location": {
+        "address": "1-103, kudiyan street, Maruthuvakkudi",
+        "postalCode": 609501,
+        "city": "kumbakonam",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/dhivakar-manivannan-455bb75a",
+          "github":"https://github.com/Dhivakar1993"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Ogai technology",
+        "position": "Senior Executive",
+        "startDate": "2024-04-12",
+        "endDate": "2024-04-30",
+        "summary": "i am the Senior executive handling Team and xml journals and books conversion",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Elumalayan polytechnic college",
+        "department": "Information technology",
+        "studyType": "fulltime",
+        "batch start year": 2009,
+        "batch end year": 2012,
+        "gpa": 8.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Html,javascript,Css",
+        "level": "beginer",
+        "project": [
+          "House applience Remote oprating system"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish,french",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Cricket,Tv",
+      }
+    ]
+  }
+  console.log(myResume);
+  
